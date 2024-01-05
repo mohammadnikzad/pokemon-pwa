17599fff282f07152859db33273dda4e2e688632
Mohammad Nikzad <nikzad.engineer@gmail.com>
2024-01-05 14:54:54 +0330

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import { getAllPokemonList } from './api/pokemon'
 
 function App() {
+  const [lists, setLists] = useState([])
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await getAllPokemonList()
+      setLists(data?.results)
+    }
+
+    fetchData()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div
+        style={{
+          marginTop: '40px',
+          justifyContent: 'space-around',
+          display: 'flex',
+          flexWrap: 'wrap',
+          width: '90%',
+          margin: 'auto',
+        }}
+      >
+        {lists?.map((list, i) => {
+          return (
+            <div
+              key={i}
+              style={{
+                width: '400px',
+                height: '330px',
+                border: '2px solid #000',
+                margin: '30px 10px',
+              }}
+            >
+              <div style={{ padding: '5px 10px' }}>
+                <p style={{ fontWeight: 'bold', textTransform: 'capitalize' }}>
+                  {list.name}
+                </p>
+              </div>
+              <img
+                src={`https://img.pokemondb.net/artwork/large/${list.name}.jpg`}
+                alt='pokemon'
+                style={{ height: '300px', width: '300px' }}
+              />
+            </div>
+          )
+        })}
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
